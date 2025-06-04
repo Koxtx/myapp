af0f3f850e3f05be5ef15dfc5ef59b9ff75c7583
[Koxtx] <corentin.lavarde@gmail.com>
2025-06-04 11:21:50 +0200

    Add Cour React Native

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [backgroundColor, setBackgroundColor] = useState("violet");
+  const [inputValue, setInputValue] = useState("");
+  const [inputResult, setInputResult] = useState([]);
+  const addText = () => {
+    setInputResult((current) => [...current, inputValue]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={[styles.container, { backgroundColor }]}>
+        <View style={styles.inputConatiner}>
+          <TextInput
+            value={inputValue}
+            onChangeText={(text) => setInputValue(text)}
+            style={styles.input}
+          />
+          <Button title="Valider" onPress={addText} />
+        </View>
+        <View style={styles.resultContainer}>
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            data={inputResult}
+            renderItem={({ item }) => (
+              <View style={styles.itemContainer}>
+                <Text style={styles.item}>{item}</Text>
+              </View>
+            )}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+
+    alignItems: "center",
+  },
+  inputConatiner: {
+    flexDirection: "row",
+    width: "100%",
+    marginHorizontal: 12,
+    marginTop: 8,
+  },
+  input: {
+    width: "80%",
+    backgroundColor: "white",
+  },
+  resultContainer: {
+    width: "100%",
+    padding: 8,
+    flex: 3,
+  },
+  itemContainer: {
+    width: "100%",
+    height: 38,
+    marginVertical: 12,
+    borderRadius: 8,
+    backgroundColor: "rgba(0,0,0,0.75)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  item: {
+    color: "white",
+    fontSize: 20,
   },
 });
